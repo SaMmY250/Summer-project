d672c74cfe5f4c4cd944e97cd837f33d83eb4c92
Sameer Maharjan <sameermaharjan982@gmail.com>
2023-05-05 19:27:11 +0545

    Updated dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,13 @@
 @extends("layouts/main")
 @section('title', 'Dashboard')
 @section('body')
-
+@include("layouts/navbar")
+<header class="content-image py-5">
+    <div class="container px-4 px-lg-5 my-5">
+        <div class="text-center text-black">
+            <h1 class="display-4 fw-bolder">Bike management system</h1>
+            <p class="lead fw-normal text-black-50 mb-0">With this shop hompeage template</p>
+        </div>
+    </div>
+</header>
 @endsection
