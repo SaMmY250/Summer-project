7e69e850eb1fd8d6bf756fbe9604dc127cf10cf8
Sameer Maharjan <sameermaharjan982@gmail.com>
2023-05-06 13:30:37 +0545

    Udpate and added

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts/main')
+@section('title', 'About us')
+@section('body')
+
+    @include('layouts/navbar')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4"><h1>About Us</h1></div>
+        </div>
+    </div>
+@endsection
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,16 +6,17 @@
         <div class="container px-4 px-lg-5 my-5">
             <div class="text-center text-black">
                 <h1 class="display-4 fw-bolder">Bike management system</h1>
-                <p class="lead fw-normal text-black-50 mb-0">With this shop hompeage template</p>
-                <button class="btn btn-primary px-4 py-3 mt-5">Service Center</button>
+                <p class="lead fw-normal text-black-50 mb-0">any problem every solution.</p>
+                <a href="{{ route('submit-form-view') }}" class="btn btn-primary px-4 py-3 mt-5">Fill Form</a>
             </div>
         </div>
     </header>
 
     <div class="container py-5">
         <div class="row">
-            <div class="col-sm-3">Our categories</div>
-            <div class="col-sm-4">Our Service</div>
+            <div class="col-sm-3"><b>Our categories</b></div>
+            <div class="col-sm-4"><b>Our Service</b></div>
+            <div class="col-sm-5"><b>Contact Us</b></div>
         </div>
 
         <div class="row mt-4">
@@ -26,6 +27,10 @@
         <div class="row mt-2">
             <div class="col-sm-3">Scooter</div>
             <div class="col-sm-4">Oil change</div>
+        </div>
+        <div class="row mt-1">
+            <div class="col-sm-5">01-425688</div>
+
         </div>
     </div>
 @endsection
