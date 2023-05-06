c8725193e14f47f818f6691f7866f492ba5341e9
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2023-05-06 13:54:09 +0545

    Commited

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,22 @@
 @extends('layouts/main')
 @section('title', 'Dashboard')
 @section('body')
+    <script src="{{ asset('js/main.js') }}"></script>
+
     @include('layouts/navbar')
+
     <header class="content-image py-5">
-        <div class="container px-4 px-lg-5 my-5">
+        <div class="px-lg-5 container my-5 px-4">
             <div class="text-center text-black">
                 <h1 class="display-4 fw-bolder">Bike management system</h1>
                 <p class="lead fw-normal text-black-50 mb-0">With this shop hompeage template</p>
-                <button class="btn btn-primary px-4 py-3 mt-5">Service Center</button>
+
+                <button class="btn btn-primary mt-5 px-4 py-3">Service Center</button>
             </div>
         </div>
     </header>
+
+
 
     <div class="container py-5">
         <div class="row">
