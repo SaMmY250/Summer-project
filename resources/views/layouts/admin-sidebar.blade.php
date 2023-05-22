74e15252d6d627dda17eec329bbad730ae3453b2
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2023-05-22 22:29:34 +0545

    siderbar updated

---
+++ resources/views/layouts/admin-sidebar.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <div class="sidebar">
+        <nav class="sidebar-nav">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="components/buttons.html"><i class="icon-puzzle"></i> Customer's
+                        Information</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="components/buttons.html"><i class="icon-puzzle"></i> Service Center
+                        Detail</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="components/social-buttons.html"><i class="icon-puzzle"></i>
+                        Settings</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="components/social-buttons.html"><i class="icon-puzzle"></i>
+                        Billing</a>
+                </li>
+            </ul>
+    </div>
+</header>
